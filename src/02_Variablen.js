79648956c388b498410cf4f3101faa9b52e27a04
soraya1618 <sorayyakhawari@gmail.com>
2024-07-04 20:18:09 +0000

    added Deklaration

--- src/02_Variablen.js
+++ src/02_Variablen.js
@@ -8,4 +8,16 @@
  * 
  */
 
+/***** Variablen 01 *******/
 
+// console.log("Hi");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze";  // Dekaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
