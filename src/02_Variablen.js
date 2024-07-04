0ee6bd420b4efbefec076ced40c063b27712d56f
soraya1618 <sorayyakhawari@gmail.com>
2024-07-04 21:19:49 +0000

    02/ 02 Wertzuweisung II + Datentypen

--- src/02_Variablen.js
+++ src/02_Variablen.js
@@ -15,9 +15,37 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | assignment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze";  // Dekaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung | assignment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze";  // Dekaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
+
+/*
+Wertzuweisung II + Datentypen
+JS ist ein untypisierte Sprache!
+ */
+
+let test;
+// test = "hi";
+// test = 2;
+
+/* undefined */
+console.log("Dattentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("-----------------")
+test = "hi"; // string
+console.log("Dattentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("-----------------")
+
+test = 2; // number
+console.log("Dattentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("-----------------")
+
+test = true;
+console.log("Dattentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("-----------------")
