a70f9550037f1f7830ce0fe1c80f7414e180833b
soraya1618 <sorayyakhawari@gmail.com>
2024-07-08 07:30:57 +0000

    10/ 02 Einzelparameter Iteration

--- src/11_Arrays_I .js
+++ src/11_Arrays_I .js
@@ -18,13 +18,15 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
