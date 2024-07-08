8de99bbd1d38fd62d30ef99ad366dfa9871b4379
soraya1618 <sorayyakhawari@gmail.com>
2024-07-08 07:21:47 +0000

    11/01 Array ...***

---
+++ src/11_Arrays_I .js
@@ -0,0 +1,42 @@
+
+/* Die Satzbau-Maschine | Arrays */
+
+/**
+ *   THEORIE
+ * 
+ */
+
+
+
+/**
+ *   PRAXIS
+ * 
+ */
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+// output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                PUNCT;
+
+    return str;
+}
+
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
+
+
