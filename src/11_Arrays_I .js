60d7299edc0650bcfb57a5e7314b4a0c7ba17d98
soraya1618 <sorayyakhawari@gmail.com>
2024-07-08 09:10:31 +0000

    11/05 02a. Theorie: Schleifen (for-schleife)

--- src/11_Arrays_I .js
+++ src/11_Arrays_I .js
@@ -28,46 +28,75 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 //output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4,word5,word6) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                word5 + GAP +
-                word6 +
-                PUNCT;
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 word5 + GAP +
+//                 word6 +
+//                 PUNCT;
 
-    return str;
-}
+//     return str;
+// }
 
 
 
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
-function output(outputStr) {
-    console.log(outputStr);
-}
+// function output(outputStr) {
+    // console.log(outputStr);
+// }
 
 /*** 01a. Funktionalität mit Array 1 */
 // Kritik | Überlegung
 // word1,word2, .... wordN :: Semantische Struktur
 // arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] + 
-                PUNCT;
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// function getSentenceArr(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   arr[0] + GAP +
+//                 arr[1] + GAP +
+//                 arr[2] + GAP +
+//                 arr[3] + GAP +
+//                 arr[4] + GAP +
+//                 arr[5] + 
+//                 PUNCT;
 
-    return str;
-}
+//     return str;
+// }
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+//for (let index = 0; index < array.length; index++) {
+    // const element = array[index];
+    
+// }
+// Inkrement (untere Grenze --> obere Grenze)
+//for (let i = 0; i < 5; i++) // i--> schleifen-Index
+// {
+//  output(i);   
+// //
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) {
+//    output(i);   
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 5) 
+// {
+//    output(i);   
+// }
+
+
+// function output(outputData) {
+//     console.log(outputData);
+// }
+
 
