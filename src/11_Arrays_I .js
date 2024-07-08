8983ba238a3e845d4174695d0535a60c3f5e4cfd
soraya1618 <sorayyakhawari@gmail.com>
2024-07-08 08:47:24 +0000

    11/ 04 01a. Funktionalität mit Array 1

--- src/11_Arrays_I .js
+++ src/11_Arrays_I .js
@@ -7,16 +7,16 @@
  */
 /*** 02. Theorie: Array */ 
 
-let arr;
+// let arr;
 // arr = new Array(); //Konstruktor
 //// arr =[]; // Literal
-arr = [2,11,7,8,12];
+// arr = [2,11,7,8,12];
 
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); //  index 0 (1. POs. im Array ) (*Index ist zeiger und zeigt erste element:2*)
-output (arr[arr.length-1])
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); //  index 0 (1. POs. im Array ) (*Index ist zeiger und zeigt erste element:2*)
+// output (arr[arr.length-1])
 
 /**
  *   PRAXIS
@@ -27,7 +27,7 @@ output (arr[arr.length-1])
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+//output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
@@ -51,4 +51,23 @@ function output(outputStr) {
     console.log(outputStr);
 }
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] + 
+                PUNCT;
+
+    return str;
+}
 
